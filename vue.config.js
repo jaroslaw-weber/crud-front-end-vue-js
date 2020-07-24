5ab767b6b37450dd4751844c5869318c4234595d
Weber Jaroslaw <weber-jaroslaw@dmm.com>
2020-07-24 15:34:47 +0900

    fix router

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 
 module.exports = {
-	publicPath: '',
-	pages: {
-		'index': {
-			entry: './src/main.js',
-			template: 'public/index.html',
-			title: 'Home',
-			chunks: ['chunk-vendors', 'chunk-common', 'index']
-		}
-	}
+	publicPath: './'
 }
